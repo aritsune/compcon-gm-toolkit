7ed392b13597d99feb3178dc31bb3ba967c4efbd
ari <sndaritsune@gmail.com>
2019-05-04 22:58:42 -0300

    begin refactor

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,9 @@
 <template>
   <div id="app">
-    <!-- <h1>LANCER</h1> -->
-    <b-container fluid id="main-zone">
-      <b-row>
-        <b-col>
-          <b-card class="bg-light">
-            <encounter-designer />
-          </b-card>
-        </b-col>
-      </b-row>
-    </b-container>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script lang="ts">
-import Vue from 'vue';
-import EncounterDesigner from './components/EncounterDesigner.vue';
-import smoothReflow from 'vue-smooth-reflow';
-
-import NPC from '@/logic/NPC.ts';
-
-export default Vue.extend({
-  mixins: [smoothReflow],
-  name: 'app',
-  components: { EncounterDesigner },
-  mounted() {
-    this.$smoothReflow &&
-      this.$smoothReflow({
-        el: '#main-zone',
-      });
-  },
-});
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #48455a;
-  margin-top: 60px;
-}
-#main-zone {
-  width: 70%;
-}
-h1 {
-  font-size: 'Arial';
-  font-weight: bolder !important;
-  font-style: italic;
-  font-size: 4em;
-  letter-spacing: 1em;
-  margin-left: 1em;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.25s;
-}
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import './plugins/vuetify'
 import App from './App.vue';
 
 Vue.config.productionTip = false;
@@ -7,11 +8,13 @@ import BootstrapVue from 'bootstrap-vue';
 import './scss/theme.scss';
 import 'vue-material-design-icons/styles.css';
 import Fragment from 'vue-fragment';
+import router from './router'
 
 Vue.use(BootstrapVue);
 
 Vue.use(Fragment.Plugin);
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app');
