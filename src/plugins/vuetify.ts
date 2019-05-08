20f55d59f244de181692d176962c047f77d8e189
ari <sndaritsune@gmail.com>
2019-05-07 21:24:08 -0300

    add system themes

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,17 +1,21 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
-import 'vuetify/src/stylus/app.styl';
+import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(Vuetify, {
   iconfont: 'mdi',
   theme: {
     primary: '#37474f',
-    secondary: '#d84315',
+    secondary: '#2979ff',
     //
     'role--striker': '#bd3f39',
     'role--tank': '#d67e08',
     'role--controller': '#398ad6',
     'role--support': '#6aa84f',
     'role--artillery': '#a64d79',
+
+    'system--weapon': '#cc0000',
+    'system--system': '#2aa780',
+    'system--trait': '#bd6579',
   },
 });
