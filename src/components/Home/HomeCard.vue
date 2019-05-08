47e8c751efa6e56b175a4ebae805575a7dc371ca
ari <sndaritsune@gmail.com>
2019-05-07 21:24:03 -0300

    tweak home

--- src/components/Home/HomeCard.vue
+++ src/components/Home/HomeCard.vue
@@ -1,19 +1,27 @@
 <template>
-        <v-card ripple :to="to" class="home-card" :class="`${color}--text ${color}--border text--darken-2`">
-            <div class="justify-center pb-0 ">
-                <h3 class="headline">
-                    {{ title }}
-                </h3>
-            </div>
-            <v-icon size="96"
+    <v-card
+        ripple
+        :to="to"
+        class="home-card"
+        :class="`${color}--text text--darken-2`"
+    >
+        <div class="justify-center pb-0 ">
+            <h3 class="headline">
+                {{ title }}
+            </h3>
+        </div>
+        <v-icon
+            size="96"
             class="py-md-5"
             v-show="$vuetify.breakpoint.mdAndUp"
             :color="color"
-            style="opacity: 0.54">{{icon}}</v-icon>
-            <div class="pt-0 grey--text text--darken-1">
-                    {{text}}
-            </div>
-        </v-card>
+            style="opacity: 0.54"
+            >{{ icon }}</v-icon
+        >
+        <div class="pt-0 grey--text text--darken-1">
+            {{ text }}
+        </div>
+    </v-card>
 </template>
 
 <script>
@@ -23,7 +31,7 @@ export default {
         title: { type: String, required: true },
         icon: { type: String, required: true },
         text: { type: String, required: true },
-        color: { type: String, default: 'black' },
+        color: { type: String, default: 'secondary' },
     }
 }
 </script>
@@ -35,6 +43,7 @@ export default {
         flex-direction: column;
         justify-content: space-around;
         padding: 1em 2em;
+        background-color: transparent !important;
     }
 
     @media screen and (min-width: 960px) {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,37 @@
 <template>
-    <v-container fluid
-     style="display: flex;"
-     align-center
-     :style="{'padding': $vuetify.breakpoint.lgAndUp ? '0 20%' : 0}"
-     fill-height
+    <v-container
+        fluid
+        style="display: flex;"
+        align-center
+        :style="{ padding: $vuetify.breakpoint.lgAndUp ? '0 20%' : 0 }"
+        fill-height
     >
         <v-layout align-center wrap>
             <v-flex xs12 sm4>
-                <HomeCard 
-                title="NPC Designer"
-                icon="mdi-account-edit"
-                text="Build NPCs to use in encounters."
-                to="npc-designer"
-                color="secondary"
+                <HomeCard
+                    title="NPC Designer"
+                    icon="mdi-account-edit"
+                    text="Build NPCs to use in encounters."
+                    to="npc-designer"
                 />
             </v-flex>
 
             <v-flex xs12 sm4>
-                <HomeCard 
-                title="Encounter Builder"
-                icon="mdi-account-multiple-plus"
-                text="Compose encounters using your NPCs."
-                to="encounter-builder"
-                color="secondary"
+                <HomeCard
+                    title="Encounter Builder"
+                    icon="mdi-account-multiple-plus"
+                    text="Compose encounters using your NPCs."
+                    to="encounter-builder"
                 />
             </v-flex>
 
             <v-flex xs12 sm4>
-                <HomeCard 
-                title="Encounter Runner"
-                icon="mdi-account-group"
-                text="Run your encounters digitally with NPC status tracking."
-                to="encounter-runner"
-                :icon-offset="13"
-                color="secondary"
+                <HomeCard
+                    title="Encounter Runner"
+                    icon="mdi-account-group"
+                    text="Run your encounters digitally with NPC status tracking."
+                    to="encounter-runner"
+                    :icon-offset="13"
                 />
             </v-flex>
         </v-layout>
